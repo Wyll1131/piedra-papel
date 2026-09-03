7b78d53021ed748a18b3d8ccdc4ef5bfab7dee4e
juliomoralestonoc-cmd <juliomoralestonoc@gmail.com>
2026-09-03 10:41:41 -0600

    fix(menu):agregre la carrera

--- piedra2/piedra2/Program.cs
+++ piedra2/piedra2/Program.cs
@@ -10,7 +10,7 @@ namespace piedra2
     {
         static void Main(string[] args)
         {
-            global::System.Console.WriteLine("Julio Humberto Morales Tonoc");
+            global::System.Console.WriteLine("Julio Humberto Morales Tonoc: Electronica");
         }
     }
 }
